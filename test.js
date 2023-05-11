0f2522f520d887da836bbbbadf9fbf6474d7f015
Shubham pipaliya <shubham.piapliya55@gmail.com>
2023-05-11 18:37:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 console.log("This is test.");
 console.log("This is test.1");
-console.log("This is test.3, test milan");
-console.log("This is test.3, test milan 2");
+console.log("This is test.3");
 console.log("This is test.4");
 console.log("This is test.5");
