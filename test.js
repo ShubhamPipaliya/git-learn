e4d6cba5a4a7d317eae727a1d9d0a063e834bc30
ShubhamPipaliya <82820281+ShubhamPipaliya@users.noreply.github.com>
2023-05-11 18:07:01 +0530

    remove line 2

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 console.log("This is test.");
 console.log("This is test.1");
-console.log("This is test.2");
 console.log("This is test.3");
 console.log("This is test.4");
 console.log("This is test.5");
